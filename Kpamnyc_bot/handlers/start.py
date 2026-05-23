3c4ca6c6d47482fc271a713a25088aec46a335eb
v729v625yk-afk <v729v625yk@privaterelay.appleid.com>
2026-05-24 01:18:11 +0200

    Update start.py

--- Kpamnyc_bot/handlers/start.py
+++ Kpamnyc_bot/handlers/start.py
@@ -1,1 +1,30 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
 
+router = Router()
+
+@router.message(Command("start"))
+async def start_command(message: Message):
+
+    text = """
+✨ Добро пожаловать в KpamnycBot!
+
+👤 Создатель бота: zexon02
+
+Здесь тебя ждёт:
+⛏️ Добыча ресурсов
+🎴 Талисманы
+👹 Боссы
+🎰 Казино
+🛒 Рынок
+🌌 Редкие предметы
+
+📌 Чтобы начать игру, используй:
+/mine
+
+❓ Для списка всех команд:
+/help
+"""
+
+    await message.answer(text)
