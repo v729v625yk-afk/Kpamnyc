2d9db4d6939902776e5c12eccea2886fa1528632
v729v625yk-afk <v729v625yk@privaterelay.appleid.com>
2026-05-24 15:14:09 +0200

    Update bot.py

--- Kpamnyc_bot/bot.py
+++ Kpamnyc_bot/bot.py
@@ -1,27 +1,10 @@
-import asyncio
-
-from dotenv import load_dotenv
-load_dotenv()
-
-from aiogram import Bot, Dispatcher
-
-from config import BOT_TOKEN
-
-from handlers.start import router as start_router
-from handlers.mine import router as mine_router
-from handlers.profile import router as profile_router
-from handlers.inventory import router as inventory_router
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(start_router)
-dp.include_router(mine_router)
-dp.include_router(profile_router)
-dp.include_router(inventory_router)
-
 async def main():
-    await dp.start_polling(bot)
+    try:
+        print("Запуск бота...")
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(f"ОШИБКА: {e}")
+        raise
 
 if __name__ == "__main__":
     asyncio.run(main())
